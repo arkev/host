791c085ce283d94dc221222fe3c10b21d14c327b
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-12-03 04:44:05 -0600

    crear la pantalla de paquetes

---
+++ paquetes.php
@@ -0,0 +1,277 @@
+<?php $active_page="paquetes"; ?>
+<?php include 'includes/head.php';?>
+<title>Arkev | Planes de hosting</title>
+<meta name="description" content="servicio de hosting de alta calidad en México, registro de dominios y diseño web creativo y compatible con celulares y tablets a precios justos">
+<meta name="keywords" content="hosting, guadalajara, monterrey, méxico, hospedaje web, dominios, diseño web, crear paginas web, registro de dominios">
+</head>
+<body id="page3">
+<div class="tail-top2">
+<?php include 'includes/header.php';?>
+
+<!-- content -->
+	<!-- content -->
+	<section id="content">
+		<div class="container">
+			<div class="inside bot-indent">
+				<div id="slogan">
+					<div class="inside">
+						<h2>PLANES DE HOSTING</h2>
+						<p>PRECIOS EN PESOS MEXICANOS CON IVA INCLUIDO.
+Hosting de alto rendimiento con tiempo de funcionamiento garantizado del 99.9%, servidores sumamente estables y veloces para tu sitio en internet y corres electrónicos, atención al cliente profesional y disponible en un amplio horario.</p>
+					</div>
+				</div>
+				<h2 class="extra">Planes de hosting</h2>
+				<div class="box extra">
+					<div class="border-right">
+						<div class="border-bot">
+							<div class="border-left">
+								<div class="left-top-corner1">
+									<div class="right-top-corner1">
+										<div class="right-bot-corner">
+											<div class="left-bot-corner">
+												<div class="inner">
+													<div class="left-indent line-ver1">
+														<div class="line-ver2">
+															<div class="line-ver3">
+																<div class="line-ver4">
+																	<article class="col-1 indent">
+																		<h4>Paquete</h4>
+																		<ul class="info-list1">
+																			<li>Disk Space</li>
+																			<li>Bandwidth</li>
+																			<li>Monthly Price</li>
+																			<li>99.9% uptime</li>
+																			<li>24/7 support</li>
+																			<li>Daily backups</li>
+																			<li>No Contract</li>
+																			<li>30 day money back guarantee</li>
+																			<li>Free Setup</li>
+																			<li>Order</li>
+																		</ul>
+																	</article>
+																	<article class="col-2 indent">
+																		<h4 class="aligncenter">Personal</h4>
+																		<ul class="info-list1 alt">
+																			<li>2,000 MB</li>
+																			<li>50,000 mb</li>
+																			<li>$35.00</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																		</ul>
+																		<div class="aligncenter"><a href="#" class="link3"><span><span>Buy Now!</span></span></a></div>
+																	</article>
+																	<article class="col-3 indent">
+																		<h4 class="aligncenter">Standard</h4>
+																		<ul class="info-list1 alt">
+																			<li>3,000mb</li>
+																			<li>65,000mb</li>
+																			<li>$45.00</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																		</ul>
+																		<div class="aligncenter"><a href="#" class="link3"><span><span>Buy Now!</span></span></a></div>
+																	</article>
+																	<article class="col-4 indent">
+																		<h4 class="aligncenter">Advanced</h4>
+																		<ul class="info-list1 alt">
+																			<li>4,000mb</li>
+																			<li>100,00mb</li>
+																			<li>$65.00</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																		</ul>
+																		<div class="aligncenter"><a href="#" class="link3"><span><span>Buy Now!</span></span></a></div>
+																	</article>
+																	<article class="col-5 indent">
+																		<h4 class="aligncenter">Other</h4>
+																		<ul class="info-list1 alt">
+																			<li>6,000mb</li>
+																			<li>150,000</li>
+																			<li>$100</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																			<li>yes</li>
+																		</ul>
+																		<div class="aligncenter"><a href="#" class="link3"><span><span>Buy Now!</span></span></a></div>
+																	</article>
+																	<div class="clear"></div>
+																</div>
+															</div>
+														</div>
+													</div>
+													<div class="border-top">
+														<div class="inner1">
+															<h4 class="extra aligncenter">You can create...</h4>
+															<div class="">
+																<div class="line-ver1 left-indent">
+																	<div class="line-ver2">
+																		<div class="line-ver3">
+																			<div class="wrap line-ver4">
+																				<article class="col-1 indent1">
+																					<ul class="info-list1">
+																						<li>Domains</li>
+																						<li>Subdomains</li>
+																						<li>FTP accounts</li>
+																					</ul>
+																				</article>
+																				<article class="col-2 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																					</ul>
+																				</article>
+																				<article class="col-3 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																					</ul>
+																				</article>
+																				<article class="col-4 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																					</ul>
+																				</article>
+																				<article class="col-5 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																						<li>unlimited</li>
+																					</ul>
+																				</article>
+																				<div class="clear"></div>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</div>
+													<div class="border-top">
+														<div class="inner1">
+															<h4 class="extra aligncenter">All accounts will have...</h4>
+															<div class="">
+																<div class="line-ver1 left-indent">
+																	<div class="line-ver2">
+																		<div class="line-ver3">
+																			<div class="wrap line-ver4">
+																				<article class="col-1 indent1">
+																					<ul class="info-list1">
+																						<li>MySQL Databases</li>
+																						<li>CGI</li>
+																						<li>PERL</li>
+																						<li>Cron</li>
+																						<li>SSI</li>
+																						<li>Frontpage</li>
+																						<li>Curl</li>
+																						<li>Image Magick</li>
+																						<li>Streaming video and music</li>
+																						<li>GD</li>
+																						<li>Python</li>
+																						<li>PHP</li>
+																					</ul>
+																				</article>
+																				<article class="col-2 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																					</ul>
+																				</article>
+																				<article class="col-3 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																					</ul>
+																				</article>
+																				<article class="col-4 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																					</ul>
+																				</article>
+																				<article class="col-5 indent1">
+																					<ul class="info-list1 alt">
+																						<li>unlimited</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																						<li>yes</li>
+																					</ul>
+																				</article>
+																				<div class="clear"></div>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+<?php include 'includes/footer.php';?>
